e541420679bf93cc24ec1917f91e11819ceaf813
Adam Ondrejkovic <adam.ondrejkovic@gmail.com>
2022-02-06 21:35:15 +0100

    Message possible

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -1,9 +1,38 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  WsResponse,
+} from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import { Socket, Server } from 'socket.io';
 
 @WebSocketGateway()
-export class AppGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+export class AppGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer() wss: Server;
+
+  private logger: Logger = new Logger('AppGateway');
+
+  afterInit(server: Server) {
+    this.logger.log('Initialized');
+  }
+
+  handleConnection(client: Socket, ...args: any[]): any {
+    this.logger.log(`Client was connected: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket): any {
+    this.logger.log(`Client was disconnected: ${client.id}`);
+  }
+
+  @SubscribeMessage('msgToServer')
+  handleMessage(client: Socket, text: string): void /*WsResponse<string>*/ {
+    this.wss.emit('msgToClient', text)
+    //return { event: 'msgToClient', data: text };
   }
 }
